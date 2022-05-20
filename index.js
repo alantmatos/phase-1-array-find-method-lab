f57225893c5cf0ff4a8c0588e4f26e69b6a19749
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-05-20 09:25:24 -0400

    commit tree 1bf0dc37ba1e5ebf2b7fed26ebfcec924d0319b8

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+
+
+
+const superbowlWin = function (record) {
+    const result = record.find( record => record.result === "W" );
+    return !!result ? result.year : undefined;
+  }
+
+  superbowlWin(record)
+
+  
+
+/*
+
+
+
+*/
